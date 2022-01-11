db4d106e5b788be7b092bd50a5a1ab49eae8890c
Hongquan Li <hqlisu@gmail.com>
2022-01-10 22:40:02 -0800

    bug fix

--- firmware/trigger_and_DAC_controller_teensy40/trigger_and_DAC_controller_teensy40.ino
+++ firmware/trigger_and_DAC_controller_teensy40/trigger_and_DAC_controller_teensy40.ino
@@ -1,24 +1,22 @@
 #include <SPI.h>
 
-// MCU - computer communication
-static const int CMD_LENGTH = 8;
-byte buffer_rx[500];
-volatile int buffer_rx_ptr;
-
-byte cmd_id = 0;
-bool mcu_cmd_execution_in_progress = false;
-
-unsigned long current_time_us;
-
-// command sets
-static const int SET_DAC = 0;
-
 // DAC80508
 const int DAC8050x_CS_pin = 10;
 const uint8_t DAC8050x_DAC_ADDR = 0x08;
 uint8_t DAC8050x_GAIN_ADDR = 0x04;
 uint8_t DAC8050x_CONFIG_ADDR = 0x03;
 uint16_t dac_value = 0;
+
+// MCU - computer communication
+static const int CMD_LENGTH = 8;
+byte buffer_rx[500];
+volatile int buffer_rx_ptr;
+byte cmd_id = 0;
+bool mcu_cmd_execution_in_progress = false;
+unsigned long current_time_us;
+
+// command sets
+static const int SET_DAC = 0;
 
 void setup() 
 {
@@ -32,16 +30,17 @@ void setup()
   
   // set the DAC80501 voltage reference and gain
   set_DAC8050x_gain();
+  set_DAC8050x_output(0,0);
+  set_DAC8050x_output(1,0);
 
   SerialUSB.begin(20000000);
-  delayMicroseconds(100000);
+  delayMicroseconds(1500000);
   while (SerialUSB.available()) 
     SerialUSB.read();
 }
 
 void loop() 
-{
-  // put your main code here, to run repeatedly:
+{  
   while (SerialUSB.available()) 
   { 
     buffer_rx[buffer_rx_ptr] = SerialUSB.read();
@@ -54,7 +53,7 @@ void loop()
       {
         case SET_DAC: 
         {
-          set_DAC8050x_output(buffer_rx[2],uint16_t(buffer_rx[3])<<8 + uint16_t(buffer_rx[4]));
+          set_DAC8050x_output(buffer_rx[2],(uint16_t(buffer_rx[3])<<8) + uint16_t(buffer_rx[4]));
           mcu_cmd_execution_in_progress = false;
           break;
         }
@@ -68,7 +67,7 @@ void set_DAC8050x_output(int channel, uint16_t value)
   SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE2));
   digitalWrite(DAC8050x_CS_pin,LOW);
   SPI.transfer(DAC8050x_DAC_ADDR+channel);
-  SPI.transfer16(dac_value);
+  SPI.transfer16(value);
   digitalWrite(DAC8050x_CS_pin,HIGH);
   SPI.endTransaction();
 }
